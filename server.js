4c6657c1a2985ec2153d4bc499716fad85cfb75c
Shawn Ellis <clownpiece>
2019-10-25 22:55:16 -0500

    Set up project

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const app = express();
+
+
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
+);
+
+
+app.listen(3000, () => console.log('Express server running on port 3000'));
